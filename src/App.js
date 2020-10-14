a4f68ff917e7de983a5e9b3477d2c55e7a953d2e
Tye <darthtye@gmail.com>
2020-10-14 04:07:58 -0400

    rona rona

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api/';
+
+import image from './images/image.svg';
+
+class App extends React.Component {
+
+    state = {
+      data: {},
+      country: '',
+    }
+
+    async componentDidMount() {
+      const fetchedData = await fetchData();
+
+      this.setState({ data: fetchedData });
+
+    }
+
+    handleCountryChange = async (country) => {
+      const data = await fetchData(country);
+
+      this.setState({ data, country: country });
+    }
+
+    render() {
+      const { data, country } = this.state;
+
+      return (
+        <div  className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
+
+          <Cards data={data} />
+
+          <Chart data={data} country={country} />
+        </div>
+      );
+    }
+}
+
+export default App;
